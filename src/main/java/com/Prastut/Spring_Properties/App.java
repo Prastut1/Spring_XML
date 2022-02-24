fda06a04d8a88ee57144abbd52c83df4a0c8506d
Prastut <prastut4321@gmail.com>
2022-02-24 16:43:04 -0500

    Adding the Project

---
+++ src/main/java/com/Prastut/Spring_Properties/App.java
@@ -0,0 +1,31 @@
+package com.Prastut.Spring_Properties;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	//Traditional way
+       Country country = new Country("India",55000);
+       System.out.println("Country name using constructor Java Object is:" + country.getName());
+       System.out.println("Country population using constructor Java Object is:"+ country.getPopulation());
+       country.setName("USA");
+       System.out.println("Country population using getter Java Object is:"+country.getName());
+       //Spring way
+       ClassPathXmlApplicationContext ac = new ClassPathXmlApplicationContext("ApplicationContext.xml");
+       Country countryBean1 = (Country)ac.getBean("country");
+       String name = countryBean1.getName();
+       long population = countryBean1.getPopulation();
+       System.out.println("Country name using Spring Bean is:" + name);
+       System.out.println("Country population using Spring Bean is:" + population);
+//       ClassPathXmlApplicationContext bc = new ClassPathXmlApplicationContext("ApplicationContext.xml");
+//       Country countryBean = (Country)bc.getBean("connttuu");
+//       String name1= countryBean.getName();
+       
+    }
+}
---
+++ src/main/java/com/Prastut/Spring_Properties/Country.java
@@ -0,0 +1,24 @@
+package com.Prastut.Spring_Properties;
+
+public class Country {
+	private String name;
+	private long population;
+	public Country(String name, long population) {
+		this.name=name;
+		this.population=population;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public long getPopulation() {
+		return population;
+	}
+	public void setPopulation(long population) {
+		this.population = population;
+	}
+	
+
+}
